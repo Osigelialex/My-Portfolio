ec652668b0e172b0411ffc7f77eb07758a6c38f9
Osigelialex <osigelialex@gmail.com>
2024-03-18 06:09:18 +0100

    Update projects.jsx and package.json, remove carousel.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import Footer from './Components/footer'
-import Header from './Components/header'
-import Projects from './Components/projects'
-import Hero from './Components/hero'
-import About from './Components/about'
-import Carousel from './Components/carousel'
-import AnimatedCursor from 'react-animated-cursor'
-
+import Footer from "./Components/footer";
+import Header from "./Components/header";
+import Projects from "./Components/projects";
+import Hero from "./Components/hero";
+import About from "./Components/about";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
-    <div className='min-h-screen bg-navy-blue p-2 font-ntr'>
-      <AnimatedCursor
-        innerSize={12}
-        outerSize={8}
-        color='14, 165, 233' />
+    <div className="min-h-screen bg-navy-blue p-2 font-ntr">
+      <AnimatedCursor innerSize={12} outerSize={8} color="14, 165, 233" />
       <Header />
       <Hero />
       <About />
-      <Carousel />
       <Projects />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
